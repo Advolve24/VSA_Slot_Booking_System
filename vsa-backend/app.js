ca90317078c3bb7c4130efb6a86aafd03d1609bb
Advolve24 <web@advolve.in>
2026-01-23 19:36:39 +0530

    updated frontend url

--- vsa-backend/app.js
+++ vsa-backend/app.js
@@ -37,7 +37,7 @@ app.use(
   })
 );
 
-
+app.options("*", cors()); 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
